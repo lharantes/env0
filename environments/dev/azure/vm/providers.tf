19fbf27260f563dea1016c85f1556168b6e83586
Rafael Afonso <ratafonso@gmail.com>
2025-03-27 09:29:09 +0000

    Changing Terraform version

--- environments/dev/azure/vm/providers.tf
+++ environments/dev/azure/vm/providers.tf
@@ -2,14 +2,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.7.0"
+      version = "~>4.4.0"
     }
   }
   required_version = "=1.9.0"
 }
 
 provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-  use_oidc = true
+  features {
+  }
 }
