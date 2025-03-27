8d7456d251d23bbf8db8a31b31e58abce9541d01
Rafael Afonso <ratafonso@gmail.com>
2025-03-27 16:52:40 +0000

    Simplify the VM code

--- environments/dev/azure/vm/main.tf
+++ environments/dev/azure/vm/main.tf
@@ -6,22 +6,22 @@ resource "azurerm_resource_group" "xyz" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_001_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.xyz.location
+  resource_group_name = azurerm_resource_group.xyz.name
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_001_name
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.xyz.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "nic" {
   name                = "azlxyzdev001-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.xyz.location
+  resource_group_name = azurerm_resource_group.xyz.name
 
   ip_configuration {
     name                          = "internal"
@@ -32,8 +32,8 @@ resource "azurerm_network_interface" "nic" {
 
 resource "azurerm_virtual_machine" "vm" {
   name                  = var.vm_name
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.xyz.location
+  resource_group_name   = azurerm_resource_group.xyz.name
   network_interface_ids = [azurerm_network_interface.nic.id]
   vm_size               = "Standard_D2_v4"
 
