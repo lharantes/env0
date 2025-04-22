71b7f87ac001520c567d83419dab72785b45c31b
Luiz Henrique Arantes <79605326+lharantes@users.noreply.github.com>
2025-04-22 10:23:44 -0300

    Update main.tf

--- environments/dev/azure/vm/main.tf
+++ environments/dev/azure/vm/main.tf
@@ -44,10 +44,10 @@ resource "azurerm_virtual_machine" "vm" {
     managed_disk_type = "Standard_LRS"
   }
 
-  storage_image_reference {
+  source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "24.04-LTS"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
 
