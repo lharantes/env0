70d3947b4767415b6f4345910a59869287c514cd
Rafael Afonso <ratafonso@gmail.com>
2025-04-23 07:07:30 +0100

    Including public ip in a single VM

--- environments/dev/azure/vm/main.tf
+++ environments/dev/azure/vm/main.tf
@@ -37,7 +37,7 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_id                  = azurerm_public_ip.publicip.id
+    public_ip_address_id          = azurerm_public_ip.publicip.id
   }
 }
 
